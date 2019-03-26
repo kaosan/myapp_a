16c7ecc995c0c7119f819b20405a7917605a8149
Kaori <xiaxulizhaiteng@gmail.com>
2019-03-26 21:37:58 +0900

    init

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def current_user?(user)
    user == current_user
 end
+def new_or_edit
+  if action_name == 'new' || action_name == 'confirm'
+    confirm_users_path
+  elsif action_name == 'edit'
+    user_path
+  end
+end
+
 end
