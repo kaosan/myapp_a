8ff5093bb8f224b535119d93171e412807f2242c
Kaori <xiaxulizhaiteng@gmail.com>
2019-03-31 15:50:29 +0900

    init

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 
 class Post < ApplicationRecord
-  mount_uploader :image, ImageUploader
-  validates :content, presence: true
-  validates :image, presence: true
   belongs_to :user
   has_many :favorite_posts, dependent: :destroy
   has_many :favorite_post_users, through: :favorite_posts, source: :user
   has_many :comments, dependent: :destroy
 
   default_scope -> { order(created_at: :desc) }
+
+  mount_uploader :image, ImageUploader
+  validates :content, presence: true
+  validates :image, presence: true
 
   def user
     return User.find_by(id: self.user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ApplicationRecord
-  mount_uploader :icon, IconUploader
-  validates :name, presence: true, length: {maximum: 30}
-  validates :email, presence: true, length: {maximum: 255},   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :icon, presence: true
-  before_validation { email.downcase! }
-
-  has_secure_password
-
-  validates :password, presence: true, length: {minimum: 6}
   has_many :posts, dependent: :destroy
 
   has_many :favorite_posts, dependent: :destroy
@@ -17,6 +8,16 @@ class User < ApplicationRecord
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  mount_uploader :icon, IconUploader
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true, length: {maximum: 255},   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :icon, presence: true
+  before_validation { email.downcase! }
+
+  has_secure_password
+
+  validates :password, presence: true, length: {minimum: 6}
 
   def follow!(other_user)
     active_relationships.create!(followed_id: other_user.id)
